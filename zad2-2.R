1fc75045ab5206ed48c50c43e38223e52c2c23dd
karpinskakarolina <37187772+karpinskakarolina@users.noreply.github.com>
2018-04-19 15:18:08 +0200

    lepiej późno niż wcale vol. 2

---
+++ zad2-2.R
@@ -0,0 +1,7 @@
+dzien <- 24
+miesiac <- (dzien * 30)
+rok <- (dzien * 365)
+IIrok <- ((miesiac - (8 * dzien) + (9 * miesiac)))
+III_IVrok <- (2 * rok)
+Vrok <- (6 * miesiac )
+WYNIK <- (IIrok + III_IVrok + Vrok)
